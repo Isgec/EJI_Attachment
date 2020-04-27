7ee02a7114b94a4a3003f63603ebdde23224b684
0340LalitGupta <lalit@isgec.co.in>
2020-04-28 03:50:14 +0530

    Initial commit

---
+++ Attachment.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Attachment.aspx.vb" Inherits="Attachment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+  <style>
+    .row_c{
+      display:flex;
+      justify-content: center;
+    }
+  </style>
+</head>
+<body style="background-color:#d7d7d7">
+    <form id="form1" runat="server">
+      <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <div class="row_c">
+      <div style="font-weight:bold; text-align: center; font-size: 20px;">Attachments</div>
+    </div>
+    <div class="row_c" style="margin-top: 20px" runat="server" id="divUploadAttachment">
+        <asp:FileUpload CssClass="button" ID="FileUpload" runat="server" AllowMultiple="true" />
+        <asp:Button ID="btnUpload" CssClass="button" Text="Upload Attachment" runat="server" OnClick="btnUpload_Click" BackColor="#53a9ff" ForeColor="White" />
+    </div>
+    <div class="row_c" style="margin-top:20px; font-size:12px; color: #646363" runat="server" id="divViewAttachment">
+      <div style="flex-grow:1;">
+
+        <asp:GridView 
+          runat="server" 
+          ID="gvAttachment" 
+          AutoGenerateColumns="false" 
+          Width="90%"
+          HeaderStyle-BackColor="White" 
+          HeaderStyle-Height="40"  
+          AllowPaging="false" 
+          DataSourceID="ODS1"
+          GridLines="Both"
+          HorizontalAlign="Center"
+          DataKeyNames="t_drid">
+          <Columns>
+            <asp:TemplateField HeaderText="File Name">
+              <ItemTemplate>
+                <%#Eval("t_fnam") %>
+              </ItemTemplate>
+              <HeaderStyle Width="30%" />
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Upload Date" ItemStyle-HorizontalAlign="Center" HeaderStyle-Width="120">
+              <ItemTemplate>
+                <%#Convert.ToDateTime(Eval("t_aton")).ToString("dd-MM-yyyy") %>
+              </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Download" ItemStyle-HorizontalAlign="Center">
+              <ItemTemplate>
+                <asp:ImageButton runat="server" ID="lnkDownload" AlternateText="dl" ImageUrl="~/download.png" CommandName="lgDownload" CommandArgument='<%# Container.DataItemIndex %>'></asp:ImageButton>
+              </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Delete" ItemStyle-HorizontalAlign="Center">
+              <ItemTemplate>
+                <asp:ImageButton runat="server" ID="lnkDelete" AlternateText="Delete" ImageUrl="~/delete.png" CommandName="lgDelete" CommandArgument='<%# Container.DataItemIndex %>' OnClientClick="return confirm('Delete File ?');"></asp:ImageButton>
+              </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Forced Delete" ItemStyle-HorizontalAlign="Center">
+              <ItemTemplate>
+                <asp:ImageButton runat="server" ID="lnkFDelete" AlternateText="Delete" ImageUrl="~/delete.png" CommandName="lgFDelete" CommandArgument='<%# Container.DataItemIndex %>' OnClientClick="return confirm('Delete File ?');"></asp:ImageButton>
+              </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="e-mail" ItemStyle-HorizontalAlign="Center" Visible="false">
+              <ItemTemplate>
+                <asp:LinkButton runat="server" ID="lnkSend" Text="Send" CommandName="lgSend" CommandArgument='<%# Container.DataItemIndex %>'></asp:LinkButton>
+              </ItemTemplate>
+            </asp:TemplateField>
+          </Columns>
+        </asp:GridView>
+      <asp:ObjectDataSource
+        ID="ODS1"
+      DataObjectTypeName = "ejiVault.EJI.ediAFile"
+      OldValuesParameterFormatString = "original_{0}"
+      SelectMethod = "GetFilesByHandleIndex"
+      TypeName = "ejiVault.EJI.ediAFile"
+      EnablePaging="False"
+      runat="server">
+      <SelectParameters >
+        <asp:QueryStringParameter Name="t_hndl" QueryStringField="AthHandle" Direction="Input" DbType="String" />
+        <asp:QueryStringParameter Name="t_indx" QueryStringField="Index" Direction="Input" DbType="String" />
+      </SelectParameters>
+      </asp:ObjectDataSource>
+      </div>
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ Attachment.aspx.vb
@@ -0,0 +1,147 @@
+﻿Imports System.Net
+Imports System.Web.Script.Serialization
+Imports ejiVault
+Partial Class Attachment
+  Inherits System.Web.UI.Page
+
+  Protected Sub btnUpload_Click(sender As Object, e As EventArgs)
+    If AthHandle <> "" AndAlso Index <> "" AndAlso AttachedBy <> "" Then
+      UploadFiles(Request, AthHandle, Index, AttachedBy)
+    Else
+      ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "", "alert('" & New JavaScriptSerializer().Serialize("Handle/Index/User may be missing.") & "');", True)
+    End If
+  End Sub
+  Private AthHandle As String = ""
+  Private Index As String = ""
+  Private AttachedBy As String = ""
+  Private ed As String = ""
+  Private Sub Attachment_Load(sender As Object, e As EventArgs) Handles Me.Load
+    SIS.SYS.Utilities.ApplicationSpacific.Initialize()
+    If Not String.IsNullOrEmpty(Request.QueryString("AthHandle")) Then
+      AthHandle = Request.QueryString("AthHandle")
+    End If
+    If Not String.IsNullOrEmpty(Request.QueryString("Index")) Then
+      Index = Request.QueryString("Index")
+    End If
+    If Not String.IsNullOrEmpty(Request.QueryString("AttachedBy")) Then
+      AttachedBy = Request.QueryString("AttachedBy")
+    End If
+    If Not String.IsNullOrEmpty(Request.QueryString("ed")) Then
+      ed = Request.QueryString("ed")
+    End If
+
+    If ed.ToLower = "n" Then
+      divUploadAttachment.Visible = False
+    End If
+    Dim Admins As String = ConfigurationManager.AppSettings("Admin")
+    If Admins.IndexOf(AttachedBy) >= 0 Then
+      gvAttachment.Columns(4).Visible = True
+    Else
+      gvAttachment.Columns(4).Visible = False
+    End If
+  End Sub
+  Public Shared Function UploadFiles(ByVal oRequest As HttpRequest, ByVal AthHandle As String, ByVal AthIndex As String, Optional ByVal CreatedBy As String = "") As List(Of EJI.ediAFile)
+    Dim mRet As New List(Of EJI.ediAFile)
+    If oRequest.Files.Count > 0 Then
+      Dim LibraryID As String = ""
+      Dim LibPath As String = ""
+      Dim tmp As EJI.ediALib = EJI.ediALib.GetActiveLibrary
+      LibPath = tmp.LibraryPath
+      LibraryID = tmp.t_lbcd
+      If Not EJI.DBCommon.IsLocalISGECVault Then
+        EJI.ediALib.ConnectISGECVault(tmp)
+      End If
+      Dim tmpFilesToDelete As New ArrayList
+
+      For I As Integer = 0 To oRequest.Files.Count - 1
+        Dim fu As HttpPostedFile = oRequest.Files(I)
+        If fu.ContentLength <= 0 Then Continue For
+        If fu.FileName = "" Then Continue For
+        Dim tmpPath As String = HttpContext.Current.Server.MapPath("~/../App_Temp")
+        Dim tmpName As String = IO.Path.GetRandomFileName()
+        Dim tmpFile As String = tmpPath & "\\" & tmpName
+        fu.SaveAs(tmpFile)
+        tmpFilesToDelete.Add(tmpFile)
+
+        Dim tmpVault As EJI.ediAFile = New EJI.ediAFile
+        Dim LibFileID As String = ""
+        With tmpVault
+          LibFileID = EJI.ediASeries.GetNextFileID
+          .t_drid = EJI.ediASeries.GetNextRecordID
+          .t_dcid = LibFileID
+          .t_hndl = AthHandle
+          .t_indx = AthIndex
+          .t_prcd = "EJIMAIN"
+          .t_fnam = IO.Path.GetFileName(fu.FileName)
+          .t_lbcd = LibraryID
+          .t_atby = CreatedBy
+          .t_aton = Now
+          .t_Refcntd = 0
+          .t_Refcntu = 0
+        End With
+        tmpVault = EJI.ediAFile.InsertData(tmpVault)
+        Try
+          If IO.File.Exists(LibPath & "\" & LibFileID) Then
+            IO.File.Delete(LibPath & "\" & LibFileID)
+          End If
+          IO.File.Move(tmpFile, LibPath & "\" & LibFileID)
+        Catch ex As Exception
+        End Try
+        mRet.Add(tmpVault)
+      Next
+      'Move command is used, Below code not required
+      'For Each str As String In tmpFilesToDelete
+      '  IO.File.Delete(str)
+      'Next
+      If Not EJI.DBCommon.IsLocalISGECVault Then
+        EJI.ediALib.DisconnectISGECVault()
+      End If
+    End If
+    Return mRet
+  End Function
+
+  Private Sub gvAttachment_RowCommand(sender As Object, e As GridViewCommandEventArgs) Handles gvAttachment.RowCommand
+    If e.CommandName.ToLower = "lgDownload".ToLower Then
+      Try
+        Dim t_drid As String = gvAttachment.DataKeys(e.CommandArgument).Values("t_drid")
+        Dim rDoc As EJI.ediAFile = EJI.ediAFile.GetFileByRecordID(t_drid)
+        Dim rLib As EJI.ediALib = EJI.ediALib.GetLibraryByID(rDoc.t_lbcd)
+        If Not EJI.DBCommon.IsLocalISGECVault Then
+          EJI.ediALib.ConnectISGECVault(rLib)
+        End If
+        Dim FilePath As String = rLib.LibraryPath & "\" & rDoc.t_dcid
+        Dim wc As WebClient = New WebClient
+        Response.Clear()
+        Response.ClearContent()
+        Response.ClearHeaders()
+        Response.Buffer = True
+        Response.AppendHeader("content-disposition", "attachment; filename=" & rDoc.t_fnam)
+        Response.ContentType = SIS.SYS.Utilities.ApplicationSpacific.ContentType(rDoc.t_fnam)
+        Dim aByte() As Byte = wc.DownloadData(FilePath)
+        Response.BinaryWrite(aByte)
+        Response.End()
+      Catch ex As Exception
+        ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "", "alert('" & New JavaScriptSerializer().Serialize(ex.Message) & "');", True)
+      End Try
+    End If
+    If e.CommandName.ToLower = "lgDelete".ToLower Then
+      Try
+        Dim t_drid As String = gvAttachment.DataKeys(e.CommandArgument).Values("t_drid")
+        EJI.ediAFile.FileDelete(t_drid)
+        gvAttachment.DataBind()
+      Catch ex As Exception
+        ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "", "alert('" & New JavaScriptSerializer().Serialize(ex.Message) & "');", True)
+      End Try
+    End If
+    If e.CommandName.ToLower = "lgFDelete".ToLower Then
+      Try
+        Dim t_drid As String = gvAttachment.DataKeys(e.CommandArgument).Values("t_drid")
+        EJI.ediAFile.ForcedFileDelete(t_drid)
+        gvAttachment.DataBind()
+      Catch ex As Exception
+        ScriptManager.RegisterClientScriptBlock(Page, Page.GetType(), "", "alert('" & New JavaScriptSerializer().Serialize(ex.Message) & "');", True)
+      End Try
+    End If
+
+  End Sub
+End Class
